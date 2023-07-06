5afa8651e97f17f2cfcd0108f867b45c52cfb4ca
Your name here <danielblandon_01@hotmail.com>
2023-07-06 09:57:31 -0500

    Se agrega las clases clientes y cuentas

---
+++ src/Cuenta_ahorros.java
@@ -0,0 +1,9 @@
+
+public class Cuenta_ahorros extends Cuenta {
+
+	public Cuenta_ahorros(int agencia, int numero) {
+		super(agencia, numero);
+		
+	}
+
+}
---
+++ src/Cuenta_corriente.java
@@ -0,0 +1,9 @@
+
+public class Cuenta_corriente extends Cuenta {
+
+	public Cuenta_corriente(int agencia , int numero) {
+		super(agencia,numero);
+		 
+	}
+
+}
